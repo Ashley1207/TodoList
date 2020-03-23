e1e0ad099553184e3c1b23c9f07d645ba9613f77
Ashley1207 <13133821409@163.com>
2020-03-23 18:06:39 +0800

    thridcommit

--- list/tests.py
+++ list/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from list.models import Content
 from list import views
+
 # Create your tests here.
 class ContentTest(TestCase):
     def test_creat(self):
@@ -10,4 +11,12 @@ class ContentTest(TestCase):
         }
         self.response = self.client.post(url,data)
         #self.assertTrue(Content.objects.exists())
+
+    def test_details(self):
+        response = self.client.get('/list/creatTodoList/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_index(self):
+        response = self.client.get('/list/deleteTodoList/')
+        self.assertEqual(response.status_code, 200)
 
